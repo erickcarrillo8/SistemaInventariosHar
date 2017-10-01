f64fa998a69962c3b3a4a871cdd8b198b1ed458c
josue <josuegarcia810@gmail.com>
2017-09-30 19:12:22 -0700

    agrege tablas de entidades

---
+++ SistemaInventarios/Entitites/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entitites
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Extension { get; set; }
+
+    }
+}
