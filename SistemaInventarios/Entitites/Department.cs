4c5cc9398826a26a7b3f4d1bb59f22e70f27f212
josue <josuegarcia810@gmail.com>
2017-09-30 19:40:39 -0700

    nuevas tablas  de entidades

---
+++ SistemaInventarios/Entitites/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entitites
+{
+    class Department
+    {
+        public string humanResources { get; set; }
+        public string wareHouse { get; set; }
+        public string complince { get; set; }
+        public string windMill { get; set; }
+        public string Production { get; set; }
+        public string Hps { get; set; }
+
+    }
+}
---
+++ SistemaInventarios/Entitites/Hardwere.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entitites
+{
+    class Hardwere
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string serial { get; set; }
+        public string MacAdress { get; set; }
+        public string Mtm  { get; set; }
+        public string Tipo { get; set; }
+
+    }
+}
